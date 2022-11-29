b135a475ccadc0ccba355db063a58dd771ff6db1
StoicInsight <gettantolcen@gmail.com>
2022-11-29 00:03:59 -0500

    .

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,22 +1,56 @@
 import React from 'react'
 import { useState } from 'react'
 
-function ButtonOne() {
-  const [count, setCount] = useState(0)
+// function ButtonOne() {
+//   const [count, setCount] = useState(0)
+//   return (
+//     <div>
+//       <button
+//       onClick={() => setCount(count + 1)}
+//       > {count}
+//       </button>
+//     </div>
+
+//   )
+// }
+
+const Display = (props) => {
   return (
     <div>
-      <button
-      onClick={() => setCount(count + 1)}
-      />
+      {props.counter}
     </div>
-
   )
 }
 
-const App = () => {
+const Button = (props) => {
+  return (
+    <div>
+      <button onClick={props.onClick}>
+        {props.text}
+      </button>
+    </div>
+  )
+}
+
+const App = (propsd) => {
+  const [counter, setCounter] = useState(0)
+
+  const increaseOne = setCounter(counter + 1)
+  const setZero = () => setCounter(0)
 
   return (
-    <div></div>
+    <div>
+      <Display counter={counter}/>
+      <Button
+        onClick={increaseOne}
+        text = 'Increase'
+      />
+      <Button
+        onClick={setZero}
+        text = 'Zero'
+      />
+      {/* <ButtonOne /> */}
+    </div>
   )
 }
 
